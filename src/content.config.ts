c6342f1149cb04f5d9f934f11604e55c052b7c13
danielquillanroxas <danielquillanr@gmail.com>
2026-03-30 16:53:28 +0300

    Add author field to posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,6 +6,7 @@ const posts = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.string(),
+    author: z.string().optional(),
     description: z.string().optional(),
     tags: z.array(z.string()).optional(),
   }),
